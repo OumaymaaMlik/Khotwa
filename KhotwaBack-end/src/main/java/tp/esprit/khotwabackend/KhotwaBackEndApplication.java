70669d20b168d37c472cd28ddbdb4e980970cac4
oumayma mlik <oumayma.mlik@esprit.tn>
2026-03-28 14:47:18 +0100

    project setup

---
+++ KhotwaBack-end/src/main/java/tp/esprit/khotwabackend/KhotwaBackEndApplication.java
@@ -0,0 +1,13 @@
+package tp.esprit.khotwabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhotwaBackEndApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KhotwaBackEndApplication.class, args);
+  }
+
+}
---
+++ KhotwaBack-end/src/test/java/tp/esprit/khotwabackend/KhotwaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package tp.esprit.khotwabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhotwaBackEndApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
